e1c85ae19caa4ae6f27283486dda94924174e81b
Lora-Borja <73265259+Lora-Borja@users.noreply.github.com>
2021-02-07 21:47:02 -0500

    Updating logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,9 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+// let map = L.map("mapid").setView([40.7, -94.5], 4);
+// Commented out the original let map code and changed to below to zone in on California.
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 // Alternative to using setView()
 // This method is useful when we need to add multiple tile layers or a background image of our map(s).
@@ -11,8 +13,19 @@ let map = L.map("mapid").setView([40.7, -94.5], 4);
     //zoom: 4
     //});
 
-// We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// Commented out the original let marker code and changed to use circleMarker() to create a light-yellow circle with black lines & a 300-meter radius of Central Los Angeles.
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1',
+    radius: 300
+ }).addTo(map);
+
+
+ // We create the tile layer that will be the background of our map.
+ // Changed the map style to "dark-v10".
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
